f3347133dc0e8c943a9b61d080b9299c0e25ba65
Vasyl Khomenko <vkhomenko@griddynamics.com>
2020-07-03 00:19:29 +0300

    Security updates

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h7%wd(*e4^8(mfsy)2j!cldf%k5y&*xpnl5pg1v#&%t42auv+s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
